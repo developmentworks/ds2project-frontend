09ab827bbb001293d6d9441443ae862314918250
Marcio silva <marcioadsworks@gmail.com>
2021-06-12 11:53:53 -0300

    start header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Header} from './components/Header'
 
 function App() {
   return (
     <div className="App">
-     <h1>Eco Poupex</h1>
+    <Header/>
+    <h1>Eco Poupex</h1>
     </div>
   );
 }
